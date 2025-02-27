e01b3ca8113ff3d839f65246785d659726256ac2
mdstrickler <michael.strickler@yale.edu>
2025-02-26 23:18:56 -0500

    Adjust dSQ examples

--- Examples/dSQ/mkjobs.py
+++ Examples/dSQ/mkjobs.py
@@ -3,7 +3,3 @@ with open('bwajobs.txt', 'w') as fp:
     for i in range(1,11):
         fp.write("module load SAMtools BWA; bwa mem ../Data/BWAIndex/genome.fa ../Data/Reads/reads_%(i)03d.fq > Results/out%(i)03d.sam && samtools sort -O bam -o Results/out%(i)03d.bam Results/out%(i)03d.sam\n" % locals())
 
-with open('bwajobsbetter.txt', 'w') as fp:
-    for i in range(1,11):
-        fp.write("module load SAMtools BWA; bwa mem ../Data/BWAIndex/genome.fa ../Data/Reads/reads_%(i)03d.fq | samtools sort -O bam -o Results/out%(i)03d.bam -\n" % locals())
-
