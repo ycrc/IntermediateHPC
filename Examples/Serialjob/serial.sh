b0e735be2d64faf6da1f877dc758d4211519adac
ayanawano <aya.nawano@yale.edu>
2026-04-13 13:19:34 -0400

    Added Serialjob (for-loop version of array jobs)

---
+++ Examples/Serialjob/serial.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --mem=10g
+#SBATCH --partition=day_amd
+
+module load SAMtools BWA
+
+for i in $(seq 1 10); do
+    index=$(printf "%03d" "$i")
+    bwa mem ../Data/BWAIndex/genome.fa ../Data/Reads/reads_${index}.fq > Results/out${index}.sam && samtools sort -O bam -o Results/out${index}.bam Results/out${index}.sam
+done
